deedc9591d01f8135df1bb200a49d8c4840ef894
Abdul Samad <samadcom797@gmail.com>
2024-04-23 09:59:56 +0500

    Second comit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function navToggle() {
   const navbar = document.querySelector("[data-navbar]");
   const navTogglers = document.querySelectorAll("[data-nav-toggler]");
   const overlay = document.querySelector("[data-overlay]");
-  const navLinks = document.querySelector("[data-nav-link]");
+  const navLinks = document.querySelectorAll("[data-nav-link]");
     
   navTogglers.forEach((toggler) => {
     toggler.addEventListener("click",(event) => {
@@ -15,7 +15,7 @@ function navToggle() {
         overlay.classList.toggle("active");
     });
   });
-
+  
   navLinks.forEach((link) => {
     link.addEventListener("click" , (event) => {
       event.preventDefault();  
@@ -27,3 +27,17 @@ function navToggle() {
 
 navToggle();
 
+
+/*============Function to render swiper JS==========*/
+
+function swiperRenderer() {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },  
+  })
+}
+
+swiperRenderer();
